c6cca2564f650f24bbe5da45d893eb7a6e858869
angel16GL <angel16.jagl@gmail.com>
2026-01-17 08:40:05 -0600

    Create AngelGon.py
    Ejercicio #1 
    
    Se crea una suma de dos números enteros y se concatena a partir de un fstring

---
+++ AngelGon.py
@@ -0,0 +1,9 @@
+#definimos dos variables con números enteros 
+num1 = 10
+num2 = 20 
+
+#Definimos una variable que contendrá el valor de la suma de las variables num1 y num2
+suma = num1 + num2 
+
+#creamos un fstring para concatenar un mensaje con el resultado de nuestra operación   
+print (f"El resultado de la operación es: {suma}")
